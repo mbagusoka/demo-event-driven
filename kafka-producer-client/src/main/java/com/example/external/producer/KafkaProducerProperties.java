69472a8f3241a3226fde23c1f8c91ad378e79932
Muhammad Bagus Oka <mekari@MEKARIs-MacBook-Pro.local>
2022-02-17 16:33:57 +0700

    Add Application Yml

--- kafka-producer-client/src/main/java/com/example/external/producer/KafkaProducerProperties.java
+++ kafka-producer-client/src/main/java/com/example/external/producer/KafkaProducerProperties.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import lombok.Data;
 
 @Configuration
-@ConfigurationProperties(prefix = "kafka")
+@ConfigurationProperties(prefix = "kafka.producer")
 @Data
 public class KafkaProducerProperties {
 
